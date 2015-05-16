86c356d69038b727ef618e6b401269868976b8a3
Rufus Mbugua <mbuguarufus@gmail.com>
2015-05-16 13:18:35 +0300

    Addded CI, Refactored Project
    Signed-off-by: Rufus Mbugua <mbuguarufus@gmail.com>

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,12 @@
+<?php
+error_reporting(1);
+require_once('REST_Controller.php');
+class MY_Controller extends REST_Controller{
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+
+}
